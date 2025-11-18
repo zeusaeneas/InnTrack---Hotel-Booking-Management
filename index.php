63ff9524b69131c51831145054c9d4c778b2d9a3
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-11-19 03:29:16 +0800

    feat: modal & initial carousel

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
 
   <title>InnTrack</title>
 
@@ -22,6 +25,19 @@
 
     .h-font {
       font-family: manrope;
+    }
+
+    /* Remove number arrows */
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+      -moz-appearance: textfield;
     }
   </style>
 </head>
@@ -53,15 +69,18 @@
           </li>
         </ul>
         <div class="d-flex" role="search">
-          <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+          <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
             Login
+          </button>
+          <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+            Register
           </button>
         </div>
       </div>
     </div>
   </nav>
 
-  <!-- Modal -->
+  <!-- Login Modal -->
   <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -70,24 +89,136 @@
             <h5 class="modal-title" d-flex align-items-center>
               <i class="bi bi-person-circle" fs-3 me-2></i> User Login
             </h5>
-            <button type="reset" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
+            <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control shado-none">
+            </div>
+            <div class="mb-4">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control shado-none">
+            </div>
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              <button type="submit" class="btn btn-dark shadow-none">Login</button>
+              <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+            </div>
           </div>
         </form>
       </div>
     </div>
   </div>
 
+  <!-- Register Modal -->
+  <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <form>
+          <div class="modal-header">
+            <h5 class="modal-title" d-flex align-items-center>
+              <i class="bi bi-person-lines-fill fs-3 me-2"></i>
+              User Registration
+            </h5>
+            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+              Note: Your details must match with your valid ID that will be required during check-in.
+            </span>
 
 
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Name</label>
+                  <input type="text" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Email</label>
+                  <input type="email" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Phone Number</label>
+                  <input type="number" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Picture</label>
+                  <input type="file" class="form-control shadow-none">
+                </div>
+                <div class="col-md-12 p-0 mb-3">
+                  <label class="form-label">Address</label>
+                  <textarea class="form-control shadow-none" rows="1"></textarea>
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Pincode</label>
+                  <input type="number" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Date of Birth</label>
+                  <input type="date" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 ps-0 mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control shadow-none">
+                </div>
+                <div class="col-md-6 p-0 mb-3">
+                  <label class="form-label">Confirm Password</label>
+                  <input type="password" class="form-control shadow-none">
+                </div>
+              </div>
+            </div>
+            <div class="text-center my-1">
+              <button type="submit" class="btn btn-dark shadow-none">Register</button>
+            </div>
+
+            <!-- <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control shado-none">
+            </div>
+            <div class="mb-4">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control shado-none">
+            </div>
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              
+              <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+            </div> -->
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <!-- Carousel -->
+  <div class="container-fluid">
+    <div class="swiper swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="images/carousel/exterior.png" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/room.png" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/gym.png" />
+        </div>
+        <div class="swiper-slide">
+          <img src="images/carousel/pool.png" />
+        </div>
+      </div>
+    </div>
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+  <script>
+    var swiper = new Swiper(".swiper-container", {
+      spaceBetween: 30,
+      effect: "parallax"
+    });
+  </script>
 </body>
 
 </html>
