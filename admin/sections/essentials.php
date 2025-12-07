7c41509eb598d3cea164be900f96fc3e1df9d316
Zeus <jeusaeneas@gmail.com>
2025-12-06 22:09:57 -0500

    feat:add section page 3

--- admin/sections/essentials.php
+++ admin/sections/essentials.php
@@ -1,5 +1,15 @@
 <?php
 
+    //frontend purpose data
+
+    define('SITE_URL', 'http://127.0.0.1/inntrack/');
+    define('ABOUT_IMG_PATH', SITE_URL.'images/about/');
+
+    //backend upload process needs this data
+
+    define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/InnTrack/images/');
+    define('ABOUT_FOLDER', 'about/');
+
     function adminLogin()
     {
         if(session_status() === PHP_SESSION_NONE){
@@ -67,5 +77,40 @@
             unset($_SESSION['alert']);
         }
     }
+
+    function uploadImage($image,$folder)
+    {
+        $valid_mime = ['image/jpeg', 'image/png', 'image/webph'];
+        $img_mime = $image['type'];
+
+        if(!in_array($img_mime,$valid_mime)){
+            return 'inv_img'; //invalid image mime or format
+        }
+        else if(($image['size']/(1024*1024))>2){
+            return 'inv_size'; //invalid size greater than 2mb
+        }
+        else{
+            $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+            $rname = 'IMG_'.random_int(11111,99999).".$ext";
+
+            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname; 
+            if(move_uploaded_file($image['tmp_name'],$img_path)){
+                return $rname;
+            }
+            else{
+                return 'upd_failed';
+            }
+        }
+    }
+
+    function deleteImage($image, $folder)
+    {
+        if(unlink(UPLOAD_IMAGE_PATH.$folder.$image)){
+            return true;
+        }
+        else{
+            return false; 
+        }
+    } 
 
 ?>
--- admin/settings.php
+++ admin/settings.php
@@ -28,7 +28,7 @@
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <h5 class="card-title m-0">General Settings</h5>
-                            <button type="button" class="btn btn-shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#general-s">
+                            <button type="button" class="btn btn-dark btn-sm" data-bs-toggle="modal" data-bs-target="#general-s">
                                 <i class="bi bi-pencil-square"></i> Edit
                             </button>
                         </div>   
@@ -72,7 +72,7 @@
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <h5 class="card-title m-0">Shutdown Website</h5>
                             <div class="form-check form-switch">
-                                <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox" role="switch" id="shutdown-toggle">
+                                <input onchange="upd_shutdown()" class="form-check-input" type="checkbox" role="switch" id="shutdown-toggle">
                             </div>
                         </div>   
                         <p class="card-text">
@@ -82,11 +82,11 @@
                 </div>
 
                 <!-- Contact details section -->
-                 <div class="card border-0 shadow-sm mb-4">
+                <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <h5 class="card-title m-0">Contact Settings</h5>
-                            <button type="button" class="btn btn-shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#contacts-s">
+                            <button type="button" class="btn btn-dark btn-sm" data-bs-toggle="modal" data-bs-target="#contacts-s">
                                 <i class="bi bi-pencil-square"></i> Edit
                             </button>
                         </div>
@@ -188,7 +188,7 @@
                                                         <span class="input-group-text"><i class="bi bi-instagram"></i></span>
                                                         <input type="text" name="ig" id="ig_inp" class="form-control shadow-none" required>
                                                     </div>
-                                                     <div class="input-group mb-3">
+                                                    <div class="input-group mb-3">
                                                         <span class="input-group-text"><i class="bi bi-tiktok"></i></span>
                                                         <input type="text" name="tt" id="tt_inp" class="form-control shadow-none" required>
                                                     </div>
@@ -209,6 +209,50 @@
                         </form>
                     </div>
                 </div>
+
+                <!-- Management Team Section -->
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Management Team</h5>
+                            <button type="button" class="btn btn-dark btn-sm" data-bs-toggle="modal" data-bs-target="#team-s">
+                                <i class="bi bi-plus-square"></i> Add
+                            </button>
+                        </div>   
+
+                        <div class="row" id="team-data">
+
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Management Team Modal -->
+                <div class="modal fade" id="team-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <form id="team_s_form">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Add Team Member</h5>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label class="form-label fw-bold">Name</label>
+                                        <input type="text" name="member_name" id="member_name_inp" class="form-control shadow-none" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label fw-bold">Picture</label>
+                                        <input type="file" name="member_picture" id="member_picture_inp" accept=".jpg, .png, .webp, .jpeg" class="form-control shadow-none" required>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" onclick="member_name_inp.value=''; member_picture_inp.value=''" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -217,6 +261,10 @@
 
     <script>
         let general_data, contacts_data;
+
+        let team_s_form = document.getElementById('team_s_form');
+        let member_name_inp = document.getElementById('member_name_inp');
+        let member_picture_inp = document.getElementById('member_picture_inp');
 
         function alert(type, msg) {
             let alertContainer = document.getElementById('alert-container');
@@ -295,7 +343,7 @@
             );
         });
 
-        function upd_shutdown(val) {
+        function upd_shutdown() {
             let isChecked = document.getElementById('shutdown-toggle').checked;
             let shutdownValue = isChecked ? 1 : 0;
             
@@ -310,6 +358,7 @@
                     } else {
                         alert('success', 'Shutdown mode off!');
                     }
+                    get_general();
                 } else {
                     alert('error', 'Failed to update shutdown status!');
                     document.getElementById('shutdown-toggle').checked = !isChecked;
@@ -387,9 +436,81 @@
             xhr.send(formData);
         });
 
+        team_s_form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            add_member();
+        });
+
+        function add_member() {
+            let data = new FormData();
+            data.append('name', member_name_inp.value);
+            data.append('picture', member_picture_inp.files[0]);
+            data.append('add_member', '');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+
+            xhr.onload = function() {
+                let modalElement = document.getElementById('team-s');
+                let modal = bootstrap.Modal.getInstance(modalElement);
+                modal.hide();
+                
+                if(this.responseText == 'inv_img') {
+                    alert('error', 'Only JPG and PNG images are allowed!');
+                }
+                else if(this.responseText == 'inv_size') {
+                    alert('error', 'Image should be less than 2MB!');
+                }
+                else if(this.responseText == 'upd_failed') {
+                    alert('error', 'Image upload failed!');
+                }
+                else if(this.responseText == 1) {
+                    alert('success', 'New member added!');
+                    member_name_inp.value = '';
+                    member_picture_inp.value = '';
+                    get_members();
+                }
+                else {
+                    alert('error', 'Server error!');
+                }
+            }
+
+            xhr.send(data);
+        }
+
+        function get_members() {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            
+            xhr.onload = function() {
+                document.getElementById('team-data').innerHTML = this.responseText;
+            }
+            xhr.send('get_members=1');
+        }
+
+        function rem_member(val) {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            
+            xhr.onload = function() {
+                if(this.responseText == 1) {
+                    alert('success', 'Member removed!');
+                    get_members();
+                }
+                else {
+                    alert('error', 'Server down!');
+                }
+            }
+
+            xhr.send('rem_member=' + val);
+        }
+
         window.onload = function() {
             get_general();
             get_contacts();
+            get_members();
         }
     </script>
 
