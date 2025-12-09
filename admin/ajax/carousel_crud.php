a8a69fe96f181c9a4cf63a3921912d28c78afa30
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-12-10 02:32:13 +0800

    refactor: rename variables for clarity

--- admin/ajax/carousel_crud.php
+++ admin/ajax/carousel_crud.php
@@ -21,21 +21,20 @@ if (isset($_POST['add_image'])) {
     }
 }
 
-if (isset($_POST['get_members'])) {
-    $res = selectAll('team_details');
+if (isset($_POST['get_carousel'])) {
+    $res = selectAll('carousel');
 
     while ($row = mysqli_fetch_assoc($res)) {
-        $path = ABOUT_IMG_PATH;
+        $path = CAROUSEL_IMG_PATH;
         echo <<<data
-                <div class="col-md-2 mb-3">
+                <div class="col-md-4 mb-3">
                     <div class="card text-bg-dark text-white">
-                        <img src="$path$row[picture]" class="card-img">
+                        <img src="$path$row[image]" class="card-img">
                         <div class="card-img-overlay text-end">
                             <button type="button" onclick="rem_member($row[sr_no])" class="btn btn-danger btn-sm shadow-none">
                                 <i class="bi bi-trash"></i> Delete
                             </button>
                         </div>
-                        <p class="card-text text-center px-3 py-2">$row[name]</p>
                     </div>
                 </div>
             data;
