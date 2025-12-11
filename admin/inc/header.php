fe7dc86f60ce59e5d3e76969a64b7b2382f4025b
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-12-12 01:28:23 +0800

    feat: added Features & Facilities

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -13,6 +13,7 @@
              <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
                  <ul class="nav nav-pills flex-column">
                      <li class="nav-item"><a class="nav-link text-white" href="dashboard.php">Dashboard</a></li>
+                     <li class="nav-item"><a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a></li>
                      <li class="nav-item"><a class="nav-link text-white" href="user_queries.php">User Queries</a></li>
                      <li class="nav-item"><a class="nav-link text-white" href="carousel.php">Carousel</a></li>
                      <li class="nav-item"><a class="nav-link text-white" href="settings.php">Settings</a></li>
