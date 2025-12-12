48ccc2ff03683e1f336687d497a4b2163e6d0db9
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-12-13 07:07:24 +0800

    feat: add registration form submission handling with AJAX

--- inc/footer.php
+++ inc/footer.php
@@ -58,5 +58,39 @@
             }
         }
     }
+
+    let register_form = document.getElementById('register-form');
+
+    register_form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let data = new FormData();
+
+        data.append('name', register_form.elements['name'].value);
+        data.append('email', register_form.elements['email'].value);
+        data.append('phonenum', register_form.elements['phonenum'].value);
+        data.append('address', register_form.elements['address'].value);
+        data.append('pincode', register_form.elements['pincode'].value);
+        data.append('dob', register_form.elements['dob'].value);
+        data.append('pass', register_form.elements['pass'].value);
+        data.append('cpass', register_form.elements['cpass'].value);
+        data.append('profile', register_form.elements['profile'].files[0]);
+        data.append('register', '');
+
+        var myModal = document.getElementById('registerModal');
+        var modal = bootstrap.Modal.getInstance(myModal);
+        modal.hide();
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "ajax/login_register", true);
+
+        xhr.onload = function() {
+
+        }
+
+        xhr.send(data);
+    });
+
+
     setActive();
 </script>
