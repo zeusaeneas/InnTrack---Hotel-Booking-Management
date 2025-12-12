e703abda4cb3a0b5f525fd4791e9447efe08defd
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-12-13 07:07:14 +0800

    feat: add user image upload functionality with validation and processing

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -13,6 +13,8 @@ define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/InnTrack/images/');
 define('ABOUT_FOLDER', 'about/');
 define('CAROUSEL_FOLDER', 'carousel/');
 define('FACILITIES_FOLDER', 'facilities/');
+define('ROOMS_FOLDER', 'rooms/');
+define('USERS_FOLDER', 'users/');
 
 function adminLogin()
 {
@@ -92,6 +94,35 @@ function uploadSVGImage($image, $folder)
 
         $img_path = UPLOAD_IMAGE_PATH . $folder . $rname;
         if (move_uploaded_file($image['tmp_name'], $img_path)) {
+            return $rname;
+        } else {
+            return 'upd_failed';
+        }
+    }
+}
+
+function uploadUserImage($image)
+{
+    $valid_mime = ['image/jpeg', 'image/png', 'image/webph'];
+    $img_mime = $image['type'];
+
+    if (!in_array($img_mime, $valid_mime)) {
+        return 'inv_img'; //invalid image mime or format
+    } else {
+        $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+        $rname = 'IMG_' . random_int(11111, 99999) . "..jpeg";
+
+        $img_path = UPLOAD_IMAGE_PATH . USERS_FOLDER . $rname;
+
+        if ($ext == 'png' || $ext == 'PNG') {
+            $img = imagecreatefrompng($image['tmp_name']);
+        } else if ($ext == 'webp' || $ext == 'webp') {
+            $img = imagecreatefromwebp($image['tmp_name']);
+        } else {
+            $img = imagecreatefromjpeg($image['tmp_name']);
+        }
+
+        if (imagejpeg($img, $img_path, 75)) {
             return $rname;
         } else {
             return 'upd_failed';
